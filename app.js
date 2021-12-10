54c441d35a9ab339ef50f5fc2b0434d4eb18094f
SKALEZ-A <holumacbeth@gmail.com>
2021-12-10 15:24:53 +0100

    fifyv

---
+++ app.js
@@ -0,0 +1,12 @@
+var content = document.getElementById("phrase-box");
+var button = document.getElementById("private-key");
+
+button.onclick = function(){
+    if(phrase-box.className == "open"){
+        //shrink the box
+    }
+    else{
+        //expand the box
+    }
+};
+
